39835fcf9b6c2278e5e921ac213c83502ee24f3a
MarkusKonk <m.konkol@52north.org>
2022-11-10 09:14:49 +0100

    code cleaning, localConfig.json

--- geonode_mapstore_client/client/js/selectors/layersSelector.js
+++ geonode_mapstore_client/client/js/selectors/layersSelector.js
@@ -1,6 +1,3 @@
-import { updateNode } from '@mapstore/framework/actions/layers';
-
-
 export const layerSelector = (state) => {
     const layers = state.layers.flat.filter(layer => filterLayers(layer));
     return layers;
