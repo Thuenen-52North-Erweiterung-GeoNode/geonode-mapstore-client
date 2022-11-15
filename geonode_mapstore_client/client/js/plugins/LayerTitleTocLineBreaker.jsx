e732df41aba20bc450d46fc606ff94444b7fa096
JohannesSchnell <j.schnell@52north.org>
2022-11-15 15:57:44 +0100

    bruch mit leerzeichen, bindestrich und hyphen NICHT MIT unterstrich

--- geonode_mapstore_client/client/js/plugins/LayerTitleTocLineBreaker.jsx
+++ geonode_mapstore_client/client/js/plugins/LayerTitleTocLineBreaker.jsx
@@ -1,66 +1,76 @@
-import React, { useEffect, useState } from 'react';
-import { connect } from 'react-redux';
-import { createPlugin } from '@mapstore/framework/utils/PluginsUtils';
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { createPlugin } from "@mapstore/framework/utils/PluginsUtils";
 
-import GroupTitle from '@mapstore/framework/components/TOC/fragments/GroupTitle';
-import GroupChildren from '@mapstore/framework/components/TOC/fragments/GroupChildren';
-import { updateNode } from '@mapstore/framework/actions/layers';
+import GroupTitle from "@mapstore/framework/components/TOC/fragments/GroupTitle";
+import GroupChildren from "@mapstore/framework/components/TOC/fragments/GroupChildren";
+import { updateNode } from "@mapstore/framework/actions/layers";
 
-import {layerSelector} from '../selectors/layersSelector';
+import { layerSelector } from "../selectors/layersSelector";
 
 function LineBreaker(props) {
-    const defaultLayers = document.getElementsByClassName("toc-default-layer-head");
-    const title = document.getElementsByClassName("toc-title");
-    const [layerGroupOpen, toggleLayerGroup] = useState(false);
-    useEffect(() => {
-        if (defaultLayers && defaultLayers.length > 0 && title && title.length > 0) {
-            Object.entries(defaultLayers).forEach(element => {
-                element[1].style.height='auto';
-                element[1].style.display='flow-root';
-            });
-            Object.entries(title).forEach(element => {
-                element[1].style.wordBreak = 'break-all';
-                element[1].style.height = 'fit-content';
-                element[1].style.width = '65%';
-                element[1].style.maxWidth = '100%';
-                element[1].style.whiteSpace = 'normal';
-            });
-        }
-
-    },[layerGroupOpen]);
-
-    GroupTitle.propTypes.onClick = () => {
-        toggleLayerGroup(!layerGroupOpen);
+  const defaultLayers = document.getElementsByClassName(
+    "toc-default-layer-head"
+  );
+  const title = document.getElementsByClassName("toc-title");
+  const [layerGroupOpen, toggleLayerGroup] = useState(false);
+  useEffect(() => {
+    if (
+      defaultLayers &&
+      defaultLayers.length > 0 &&
+      title &&
+      title.length > 0
+    ) {
+      Object.entries(defaultLayers).forEach((element) => {
+        element[1].style.height = "auto";
+        element[1].style.display = "flow-root";
+      });
+      Object.entries(title).forEach((element) => {
+        element[1].style.wordBreak = "normal";
+        element[1].style.overflowWrap = "anywhere";
+        element[1].style.hyphens = "auto";
+        element[1].style.height = "fit-content";
+        element[1].style.width = "65%";
+        element[1].style.maxWidth = "100%";
+        element[1].style.whiteSpace = "normal";
+      });
     }
+  }, [layerGroupOpen]);
 
-    const [toggleTooltip, toggleTooltipDeactivator] = useState(true);
-    
-    useEffect(() => {
-        if(props.layers.length > 0){
-            props.layers.forEach(element => {
-                if (element.tooltipOptions != 'none') {
-                    props.deactivateTooltipOptions(element.id, 'layer', {tooltipOptions: 'none'});        
-                }
- 
-            });
+  GroupTitle.propTypes.onClick = () => {
+    toggleLayerGroup(!layerGroupOpen);
+  };
+
+  const [toggleTooltip, toggleTooltipDeactivator] = useState(true);
+
+  useEffect(() => {
+    if (props.layers.length > 0) {
+      props.layers.forEach((element) => {
+        if (element.tooltipOptions != "none") {
+          props.deactivateTooltipOptions(element.id, "layer", {
+            tooltipOptions: "none",
+          });
         }
-    }, [toggleTooltip]);
-
-    GroupChildren.propTypes.onSort = () => {
-        toggleTooltipDeactivator(!toggleTooltip)
+      });
     }
+  }, [toggleTooltip]);
 
-    return (
-        <div></div>
-    )
+  GroupChildren.propTypes.onSort = () => {
+    toggleTooltipDeactivator(!toggleTooltip);
+  };
+
+  return <div></div>;
 }
 
-const LineBreakerPlugin = connect((state) => ({
-    layers: layerSelector(state)
-}), {
+const LineBreakerPlugin = connect(
+  (state) => ({
+    layers: layerSelector(state),
+  }),
+  {
     deactivateTooltipOptions: updateNode,
-})(LineBreaker);
+  }
+)(LineBreaker);
 
-export default createPlugin('LayerTitleTocLineBreaker', {
-    component: LineBreakerPlugin,
+export default createPlugin("LayerTitleTocLineBreaker", {
+  component: LineBreakerPlugin,
 });
