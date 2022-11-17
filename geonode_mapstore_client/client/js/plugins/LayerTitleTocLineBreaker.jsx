6e903996c09fe4373859f2a16908a42e5886fbf2
JohannesSchnell <j.schnell@52north.org>
2022-11-17 15:28:46 +0100

    umbruch wie im toc-layer auch in toc-gruppen

--- geonode_mapstore_client/client/js/plugins/LayerTitleTocLineBreaker.jsx
+++ geonode_mapstore_client/client/js/plugins/LayerTitleTocLineBreaker.jsx
@@ -12,20 +12,39 @@ function LineBreaker(props) {
   const defaultLayers = document.getElementsByClassName(
     "toc-default-layer-head"
   );
+  const groupLayers = document.getElementsByClassName("toc-default-group-head");
   const title = document.getElementsByClassName("toc-title");
+  const groupTitle = document.getElementsByClassName("toc-group-title");
   const [layerGroupOpen, toggleLayerGroup] = useState(false);
   useEffect(() => {
     if (
       defaultLayers &&
       defaultLayers.length > 0 &&
       title &&
-      title.length > 0
+      title.length > 0 &&
+      groupLayers &&
+      groupLayers.length > 0 &&
+      groupTitle &&
+      groupTitle.length > 0
     ) {
       Object.entries(defaultLayers).forEach((element) => {
         element[1].style.height = "auto";
         element[1].style.display = "flow-root";
       });
+      Object.entries(groupLayers).forEach((element) => {
+        element[1].style.height = "auto";
+        element[1].style.display = "flow-root";
+      });
       Object.entries(title).forEach((element) => {
+        element[1].style.wordBreak = "normal";
+        element[1].style.overflowWrap = "anywhere";
+        element[1].style.hyphens = "auto";
+        element[1].style.height = "fit-content";
+        element[1].style.width = "65%";
+        element[1].style.maxWidth = "100%";
+        element[1].style.whiteSpace = "normal";
+      });
+      Object.entries(groupTitle).forEach((element) => {
         element[1].style.wordBreak = "normal";
         element[1].style.overflowWrap = "anywhere";
         element[1].style.hyphens = "auto";
