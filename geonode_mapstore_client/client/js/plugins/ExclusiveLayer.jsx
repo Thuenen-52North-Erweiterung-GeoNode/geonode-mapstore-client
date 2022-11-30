85493770bf67534b8e4be5c3687710ef9ac60a12
Markus Konkol <m.konkol@52north.org>
2022-11-30 18:02:23 +0100

    address bug: MS broke after adding another dataset, doesn't break anymore but sometimes requires a few clicks to turn on/off layers from exclsuive groups

--- geonode_mapstore_client/client/js/plugins/ExclusiveLayer.jsx
+++ geonode_mapstore_client/client/js/plugins/ExclusiveLayer.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import head from 'lodash/head';
 import isObject from 'lodash/isObject';
 import { connect } from 'react-redux';
@@ -7,25 +7,41 @@ import { layerSelector, groupSelector } from '../selectors/layersSelectors';
 import { updateNode } from '@mapstore/framework/actions/layers';
 
 function ExclusiveLayer(props) {
-    
+    const layers = props.layers;
+    const previousLayers = useRef("");
     const [ clicked, setClicked ] = useState(false);
-    window.onclick = () => {
-        setClicked(true);
+    window.onclick = (e) => {
+        //console.log(e)
+        if (e.target.className.includes("glyphicon-eye-open")) {
+            setClicked(true);
+        }
     }
-    props.layers.forEach(element => {
-        useEffect(() => {
-            const correspondingGroup = head(props.groups.filter((group) => isObject(group) && group.id === element.group));
-            if ( correspondingGroup && correspondingGroup.exclusiveLayer && clicked ) {
-                const allLayersInCorrespondingGroup = props.layers.filter((layer) => element.group === layer.group);
-                allLayersInCorrespondingGroup.forEach( layer => {
-                    if ( layer.id != element.id ) {
-                        props.changeLayerVisibility(layer.id, 'layer', {visibility: false});
-                    }
-                });
-                setClicked(false);
+
+    useEffect(() => {
+        previousLayers.current = layers;
+    }, [clicked])
+
+    if ( clicked && layers.length === previousLayers.current.length ) {
+        for ( let i = 0; i < layers.length; i++ ) {
+            //console.log(layers[i].visibility !== previousLayers.current[i].visibility)
+            if ( layers[i].visibility !== previousLayers.current[i].visibility ) {
+                const correspondingGroup = head(props.groups.filter((group) => isObject(group) && group.id === layers[i].group));   
+                if ( correspondingGroup && correspondingGroup.exclusiveLayer ) {                    
+                    const allLayersInCorrespondingGroup = props.layers.filter((layer) => layers[i].group === layer.group);
+                    allLayersInCorrespondingGroup.forEach( layer => {
+                        //console.log(layers[i].title, layer.id != layers[i].id)
+                        if ( layer.id != layers[i].id ) {
+                            props.changeLayerVisibility(layer.id, 'layer', {visibility: false});
+                        }
+                        //props.changeLayerVisibility(layers[i].id, 'layer', {visibility: true});
+                    });
+                    //console.log("-------------------------------------------------------------")
+                    setClicked(false);
+                }
             }
-        }, [element.visibility]);
-    });
+        }
+    }
+
     return (
         <div></div>
     )
