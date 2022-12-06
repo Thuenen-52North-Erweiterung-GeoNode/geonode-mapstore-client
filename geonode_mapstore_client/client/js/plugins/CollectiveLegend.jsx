bade44572ac5238c88e4722d64710b919e7ff615
Markus Konkol <m.konkol@52north.org>
2022-12-06 10:39:00 +0100

    add style description to legend

--- geonode_mapstore_client/client/js/plugins/CollectiveLegend.jsx
+++ geonode_mapstore_client/client/js/plugins/CollectiveLegend.jsx
@@ -1,10 +1,10 @@
 import React, {useEffect, useState, useCallback} from 'react';
 import { connect, createPlugin } from '@mapstore/framework/utils/PluginsUtils';
-import {get} from 'lodash';
+import { get } from 'lodash';
+import { Glyphicon, Tooltip } from 'react-bootstrap';
 
 import ResizableModal from '@mapstore/framework/components/misc/ResizableModal';
 import WMSLegend from '@mapstore/framework/components/TOC/fragments/WMSLegend';
-import { Glyphicon, Tooltip } from 'react-bootstrap';
 import OverlayTrigger from '@mapstore/framework/components/misc/OverlayTrigger';
 import Button from '../../MapStore2/web/client/components/misc/Button';
 import Message from '@mapstore/framework/components/I18N/Message';
@@ -25,7 +25,7 @@ import { getStyleCodeByName } from '@mapstore/framework/api/geoserver/Styles';
 
 function CollectiveLegendModal(props) {
     return ( 
-        props.collectiveLegend ? 
+        props.collectiveLegend && props.layers ? 
             <React.Fragment>
                 {
                     <ResizableModal
@@ -38,16 +38,15 @@ function CollectiveLegendModal(props) {
                         fitContent={true}
                     >
                         <div className='collectiveLegendModal'>
-                            <h1 style={{marginBottom: "5%"}}><b> Collective Legend of visible layers </b></h1>
-                            {props.layers && props.layers.length > 0 ? 
+                            <h1 className='collectiveLegendHeader'><b>Collective Legend of visible layers</b></h1>
+                            {props.layers.length > 0 ? 
                                 (props.layers.reverse().map((layer)=> (
                                     layer.visibility ?
-                                    <div style = {{marginBottom:"5%"}}>
+                                    <div className='legendLayer'>
                                         <p><b>Layer:</b> {layer.title}</p>
                                         {props.styleeditor && props.styleeditor.service ?
                                         <StyleInformation layer={layer} editor={props.styleeditor} /> : null
                                         }
-
                                         <WMSLegend node={layer} />
                                     </div>
                                     : null
@@ -68,31 +67,36 @@ function CollectiveLegendModal(props) {
 
 function StyleInformation(props) {
     if (props.editor.service) {
-        
-    const [posts, setPosts] = useState([]);
-    useEffect(()=>{
-        getStyleCodeByName({
-            baseUrl: props.editor.service.baseUrl,
-            styleName: props.layer.name,
-        }).then((style) => {
-            
-            if (style.code) {
-                const parsedStyleCode = new DOMParser().parseFromString(style.code, "text/xml");
-                let title = "";
-                if ( parsedStyleCode.querySelectorAll("Title")[0] ) {
-                    title = parsedStyleCode.querySelectorAll("Title")[0].textContent;
+        const [styleInfo, setStyleInfo] = useState([]);
+        useEffect(()=>{
+            getStyleCodeByName({
+                baseUrl: props.editor.service.baseUrl,
+                styleName: props.layer.name,
+            }).then((style) => {
+                if (style.code) {
+                    const parsedStyleCode = new DOMParser().parseFromString(style.code, "text/xml");
+                    let title = "";
+                    let abstract = "";
+                    if ( parsedStyleCode.querySelectorAll("Title")[0] ) {
+                        title = parsedStyleCode.querySelectorAll("Title")[0].textContent;
+                    }
+                    if ( parsedStyleCode.querySelectorAll("Abstract")[0] ) {
+                        abstract = parsedStyleCode.querySelectorAll("Abstract")[0].textContent;
+                    }
+                    setStyleInfo({
+                        "title": title,
+                        "abstract": abstract,
+                    });
                 }
-                setPosts(title);
-            }
+            })
         })
-    })
-    return(
-        <div>
-            <p>Style title: {posts}</p>
-            <p>Style description</p>
-        </div>
+        return(
+            <div>
+                <p><b>Style title:</b> {styleInfo.title}</p>
+                <p><b>Style description:</b> {styleInfo.abstract}</p>
+            </div>
     )} else {
-        return <div>Nein</div>;
+        return null;
     }
 };
 
