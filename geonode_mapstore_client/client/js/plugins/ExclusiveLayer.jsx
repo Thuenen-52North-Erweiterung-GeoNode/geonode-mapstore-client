7e66a06d2e64a8df9de13dd4e3c739661027afa7
Markus Konkol <m.konkol@52north.org>
2022-12-07 12:27:02 +0100

    make excl. layer work for 'Default' groups

--- geonode_mapstore_client/client/js/plugins/ExclusiveLayer.jsx
+++ geonode_mapstore_client/client/js/plugins/ExclusiveLayer.jsx
@@ -7,6 +7,8 @@ import { layerSelector, groupSelector } from '../selectors/layersSelectors';
 import { updateNode } from '@mapstore/framework/actions/layers';
 
 function ExclusiveLayer(props) {
+    //console.log(props.layers)
+    //console.log("start excl. layer")
     const layers = props.layers;
     const previousLayers = useRef("");
     const [ clicked, setClicked ] = useState(false);
@@ -17,14 +19,21 @@ function ExclusiveLayer(props) {
     }
 
     useEffect(() => {
+        //console.log("useEffect before init", previousLayers.current)
         previousLayers.current = layers;
+        //console.log("useEffect after init", previousLayers.current)
     }, [clicked])
 
+    //console.log("layers length check",props.layers.length, previousLayers.current.length, props.layers.length === previousLayers.current.length) // && previous.current
+
     if ( clicked && layers.length === previousLayers.current.length ) {
+        //console.log("first if condition", clicked && layers.length === previousLayers.current.length)
         for ( let i = 0; i < layers.length; i++ ) {
             if ( layers[i].visibility !== previousLayers.current[i].visibility ) {
                 const correspondingGroup = getCorrespondingGroup( props, layers[i] )
+                //console.log("corresponding group", correspondingGroup )
                 if ( correspondingGroup && correspondingGroup.exclusiveLayer ) {                    
+                    //console.log("second if condition")
                     const allLayersInCorrespondingGroup = props.layers.filter((layer) => layers[i].group === layer.group);
                     allLayersInCorrespondingGroup.forEach( layer => {
                         if ( layer.id != layers[i].id ) {
@@ -43,7 +52,12 @@ function ExclusiveLayer(props) {
 }
 
 const getCorrespondingGroup = ( props, layer ) => {
-    return head(props.groups.filter((group) => isObject(group) && group.id === layer.group));
+    const corrGr = head(props.groups.filter((group) => isObject(group) && group.id === layer.group));
+    if ( corrGr !== undefined ) {
+        return corrGr;
+    } else {
+        return head(props.groups.filter((group) => isObject(group) && group.id === "Default"));
+    }
 }
 
 const ExclusiveLayerPlugin = connect((state) => ({
