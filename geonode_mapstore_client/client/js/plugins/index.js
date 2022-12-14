49be05da88b50f967b34862b84afce1de6b956e5
Henning Bredel <h.bredel@52north.org>
2022-12-14 10:09:09 +0000

    Apply line break via css instead of plugin

--- geonode_mapstore_client/client/js/plugins/index.js
+++ geonode_mapstore_client/client/js/plugins/index.js
@@ -104,10 +104,6 @@ function splitLazyAndStaticPlugins(pluginsDefinition) {
 // provides a way to import dynamically plugins similar to extensions
 
 export const plugins = {
-    LayerTitleTocLineBreakerPlugin: toLazyPlugin(
-        'LayerTitleTocLineBreaker',
-        () => import(/* webpackChunkName: 'plugins/line-breaker-plugin' */ '@js/plugins/LayerTitleTocLineBreaker')
-    ),
     LayerDownloadPlugin: toLazyPlugin(
         'LayerDownload',
         () => import(/* webpackChunkName: 'plugins/layer-download' */ '@mapstore/framework/plugins/LayerDownload'),
