2a8d8197324f8f68853a4c2e65258b89ac2ee6e5
JohannesSchnell <j.schnell@52north.org>
2023-02-21 15:01:21 +0100

    init 2

--- geonode_mapstore_client/client/js/plugins/SyncResource.jsx
+++ geonode_mapstore_client/client/js/plugins/SyncResource.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+/* import React from "react";
 import { createPlugin } from "../../MapStore2/web/client/utils/PluginsUtils";
 import { connect } from "react-redux";
 import { createSelector } from "reselect";
 import Message from "../../MapStore2/web/client/components/I18N/Message";
 import Button from "../components/Button/Button";
 import ResizableModal from "../../MapStore2/web/client/components/misc/ResizableModal";
-import Portal from "../../MapStore2/web/client/components/misc/Portal";
+import Portal from "../../MapStore2/web/client/components/misc/Portal"; */
+import React from 'react';
+import { createPlugin } from '@mapstore/framework/utils/PluginsUtils';
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+import Message from '@mapstore/framework/components/I18N/Message';
+import Button from '@js/components/Button';
+import ResizableModal from '@mapstore/framework/components/misc/ResizableModal';
+import Portal from '@mapstore/framework/components/misc/Portal';
+import { setControlProperty } from '@mapstore/framework/actions/controls';
+import { getResourceData } from '@js/selectors/resource';
+import { processResources } from '@js/actions/gnresource';
+import ResourceCard from '@js/components/ResourceCard';
+import { ProcessTypes } from '@js/utils/ResourceServiceUtils';
+import Loader from '@mapstore/framework/components/misc/Loader';
+
 
 function SyncResourcePlugin({
   enabled,
@@ -50,6 +65,39 @@ function SyncResourcePlugin({
         onClose={loading ? null : () => onClose()}
       >
         ACHTUNG ACHTUNG
+
+        <ul
+                    className="gn-card-grid"
+                    style={{
+                        listStyleType: 'none',
+                        padding: '0.5rem',
+                        margin: 0
+                    }}
+                >
+                    {resources.map((data, idx) => {
+                        return (
+                            <li style={{ padding: '0.25rem 0' }} key={data.pk + '-' + idx}>
+                                <ResourceCard data={data} layoutCardsStyle="list" readOnly/>
+                            </li>
+                        );
+                    })}
+                </ul>
+                {loading && <div
+                    style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%',
+                        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+                        zIndex: 2,
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Loader size={70}/>
+                </div>}
       </ResizableModal>
     </Portal>
   );
