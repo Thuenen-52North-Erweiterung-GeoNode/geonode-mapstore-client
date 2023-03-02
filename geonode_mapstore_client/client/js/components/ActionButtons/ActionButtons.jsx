8111e91d9cca88c0ae5bcb30df95a9cc5b44de67
JohannesSchnell <j.schnell@52north.org>
2023-03-02 15:32:19 +0100

    ende

--- geonode_mapstore_client/client/js/components/ActionButtons/ActionButtons.jsx
+++ geonode_mapstore_client/client/js/components/ActionButtons/ActionButtons.jsx
@@ -5,30 +5,34 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React, { useRef } from 'react';
-import PropTypes from 'prop-types';
-import Dropdown from '@js/components/Dropdown';
-import Message from '@mapstore/framework/components/I18N/Message';
-import FaIcon from '@js/components/FaIcon';
-import { canCopyResource } from '@js/utils/ResourceUtils';
+import React, { useRef } from "react";
+import PropTypes from "prop-types";
+import Dropdown from "@js/components/Dropdown";
+import Message from "@mapstore/framework/components/I18N/Message";
+import FaIcon from "@js/components/FaIcon";
+import { canCopyResource } from "@js/utils/ResourceUtils";
 
 // this is a workaround based on the current structure of actions in card options
 // new version will centralize this logic inside the correspondent plugins
 const checkAction = {
-    'delete': (resource) => !!resource?.perms?.includes('delete_resourcebase'),
-    // we assume tha the add_resource check has been checked in parent elements
-    'copy': (resource) => canCopyResource(resource, { perms: ['add_resource'] }),
-    'download': (resource) => !!(resource?.download_url && resource?.perms?.includes('download_resourcebase'))
+  delete: (resource) => !!resource?.perms?.includes("delete_resourcebase"),
+  // we assume tha the add_resource check has been checked in parent elements
+  copy: (resource) => canCopyResource(resource, { perms: ["add_resource"] }),
+  download: (resource) =>
+    !!(
+      resource?.download_url &&
+      resource?.perms?.includes("download_resourcebase")
+    ),
 };
 function ActionButtons({
-    options,
-    actions,
-    onAction,
-    resource,
-    buildHrefByTemplate,
-    onDownload
+  options,
+  actions,
+  onAction,
+  resource,
+  buildHrefByTemplate,
+  onDownload,
 }) {
-
+  /* 
 options.push({
     "type": "button",
     "action": "sync",
@@ -41,79 +45,82 @@ options.push({
             "value": "delete_resourcebase"
         }
     ]
-})
-console.log(resource)
-console.log(actions, "XXXXXXX")
-    const containerNode = useRef();
-    const dropdownClassName = 'gn-card-dropdown';
-    const dropdownNode = containerNode?.current?.querySelector(`.${dropdownClassName}`);
-    const isDropdownEmpty = (dropdownNode?.children?.length || 0) === 0;
+}) */
+  console.log(resource);
+  console.log(actions, "XXXXXXX");
+  const containerNode = useRef();
+  const dropdownClassName = "gn-card-dropdown";
+  const dropdownNode = containerNode?.current?.querySelector(
+    `.${dropdownClassName}`
+  );
+  const isDropdownEmpty = (dropdownNode?.children?.length || 0) === 0;
 
-    return (
-        <div
-            ref={containerNode}
-            className="gn-resource-action-buttons"
-            onClick={event => event.stopPropagation()}
-            style={isDropdownEmpty ? { display: 'none' } : {}}
+  return (
+    <div
+      ref={containerNode}
+      className="gn-resource-action-buttons"
+      onClick={(event) => event.stopPropagation()}
+      style={isDropdownEmpty ? { display: "none" } : {}}
+    >
+      <Dropdown className="gn-card-options" pullRight>
+        <Dropdown.Toggle
+          id={`gn-card-options-${resource.pk2 || resource.pk}`}
+          variant="default"
+          size="sm"
+          noCaret
         >
-            <Dropdown className="gn-card-options" pullRight>
-                <Dropdown.Toggle
-                    id={`gn-card-options-${resource.pk2 || resource.pk}`}
-                    variant="default"
-                    size="sm"
-                    noCaret
+          <FaIcon name="ellipsis-v" />
+        </Dropdown.Toggle>
+        <Dropdown.Menu className={dropdownClassName}>
+          {options.map((opt) => {
+            if (
+              (opt.type === "button" && actions[opt.action]) ||
+              opt.action === "download"
+            ) {
+              const checkFunc = checkAction[opt.action];
+              return !checkFunc || checkFunc(resource) ? (
+                <Dropdown.Item
+                  key={opt.action}
+                  onClick={() =>
+                    opt.action !== "download"
+                      ? onAction(actions[opt.action], [resource])
+                      : onDownload(resource)
+                  }
                 >
-                    <FaIcon name="ellipsis-v" />
-                </Dropdown.Toggle>
-                <Dropdown.Menu className={dropdownClassName}>
-                    {options.map((opt) => {
-                        if ((opt.type === 'button' && actions[opt.action]) || opt.action === 'download') {
-                            const checkFunc = checkAction[opt.action];
-                            return (!checkFunc || checkFunc(resource))
-                                ? (<Dropdown.Item
-                                    key={opt.action}
-                                    onClick={() =>
-                                        opt.action !== 'download' ? onAction(actions[opt.action], [
-                                            resource
-                                        ]) : onDownload(resource)
-                                    }
-                                >
-                                    <FaIcon name={opt.icon} />{' '}
-                                    <Message msgId={opt.labelId} />
-                                </Dropdown.Item>)
-                                : null;
-                        }
+                  <FaIcon name={opt.icon} /> <Message msgId={opt.labelId} />
+                </Dropdown.Item>
+              ) : null;
+            }
 
-                        return (
-                            <Dropdown.Item
-                                key={opt.href}
-                                href={buildHrefByTemplate(resource, opt.href)}
-                            >
-                                <FaIcon name={opt.icon} />{' '}
-                                <Message msgId={opt.labelId} />
-                            </Dropdown.Item>
-                        );
-                    })}
-                </Dropdown.Menu>
-            </Dropdown>
-        </div>
-    );
+            return (
+              <Dropdown.Item
+                key={opt.href}
+                href={buildHrefByTemplate(resource, opt.href)}
+              >
+                <FaIcon name={opt.icon} /> <Message msgId={opt.labelId} />
+              </Dropdown.Item>
+            );
+          })}
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
+  );
 }
 
 ActionButtons.propTypes = {
-    options: PropTypes.array,
-    actions: PropTypes.object,
-    onAction: PropTypes.func,
-    resource: PropTypes.object,
-    buildHrefByTemplate: PropTypes.func
+  options: PropTypes.array,
+  actions: PropTypes.object,
+  onAction: PropTypes.func,
+  resource: PropTypes.object,
+  buildHrefByTemplate: PropTypes.func,
 };
 
 ActionButtons.defaultProps = {
-    options: [],
-    actions: {},
-    resource: {},
-    onAction: () => {},
-    buildHrefByTemplate: () => {}
+  options: [],
+  actions: {},
+  resource: {},
+  onAction: () => {},
+  buildHrefByTemplate: () => {},
 };
 
 export default ActionButtons;
--- geonode_mapstore_client/client/js/plugins/SyncResource.jsx
+++ geonode_mapstore_client/client/js/plugins/SyncResource.jsx
@@ -6,21 +6,20 @@ import Message from "../../MapStore2/web/client/components/I18N/Message";
 import Button from "../components/Button/Button";
 import ResizableModal from "../../MapStore2/web/client/components/misc/ResizableModal";
 import Portal from "../../MapStore2/web/client/components/misc/Portal"; */
-import React from 'react';
-import { createPlugin } from '@mapstore/framework/utils/PluginsUtils';
-import { connect } from 'react-redux';
-import { createSelector } from 'reselect';
-import Message from '@mapstore/framework/components/I18N/Message';
-import Button from '@js/components/Button';
-import ResizableModal from '@mapstore/framework/components/misc/ResizableModal';
-import Portal from '@mapstore/framework/components/misc/Portal';
-import { setControlProperty } from '@mapstore/framework/actions/controls';
-import { getResourceData } from '@js/selectors/resource';
-import { processResources } from '@js/actions/gnresource';
-import ResourceCard from '@js/components/ResourceCard';
-import { ProcessTypes } from '@js/utils/ResourceServiceUtils';
-import Loader from '@mapstore/framework/components/misc/Loader';
-
+import React from "react";
+import { createPlugin } from "@mapstore/framework/utils/PluginsUtils";
+import { connect } from "react-redux";
+import { createSelector } from "reselect";
+import Message from "@mapstore/framework/components/I18N/Message";
+import Button from "@js/components/Button";
+import ResizableModal from "@mapstore/framework/components/misc/ResizableModal";
+import Portal from "@mapstore/framework/components/misc/Portal";
+import { setControlProperty } from "@mapstore/framework/actions/controls";
+import { getResourceData } from "@js/selectors/resource";
+import { processResources } from "@js/actions/gnresource";
+import ResourceCard from "@js/components/ResourceCard";
+import { ProcessTypes } from "@js/utils/ResourceServiceUtils";
+import Loader from "@mapstore/framework/components/misc/Loader";
 
 function SyncResourcePlugin({
   enabled,
@@ -29,6 +28,7 @@ function SyncResourcePlugin({
   onSync = () => {},
   loading,
 }) {
+  const size = 50;
   console.log("finger");
   return (
     <Portal>
@@ -37,67 +37,50 @@ function SyncResourcePlugin({
         show={enabled}
         fitContent
         clickOutEnabled
-        buttons={
-          loading
-            ? []
-            : [
-                {
-                  text: (
-                    <Message
-                      msgId="gnviewer.deleteResourceNo"
-                      msgParams={{ count: resources.length }}
-                    />
-                  ),
-                  onClick: () => onClose(),
-                },
-                {
-                  text: (
-                    <Message
-                      msgId="gnviewer.deleteResourceYes"
-                      msgParams={{ count: resources.length }}
-                    />
-                  ),
-                  bsStyle: "danger",
-                  onClick: () => onSync(resources, redirectTo),
-                },
-              ]
-        }
+        buttons={[
+          { text: "ne danke schliessen ", onClick: () => onClose() },
+          {
+            text: "jawollo teilen",
+            onClick: () => console.log("share the shit yo"),
+          },
+        ]}
         onClose={loading ? null : () => onClose()}
       >
         ACHTUNG ACHTUNG
-
         <ul
-                    className="gn-card-grid"
-                    style={{
-                        listStyleType: 'none',
-                        padding: '0.5rem',
-                        margin: 0
-                    }}
-                >
-                    {resources.map((data, idx) => {
-                        return (
-                            <li style={{ padding: '0.25rem 0' }} key={data.pk + '-' + idx}>
-                                <ResourceCard data={data} layoutCardsStyle="list" readOnly/>
-                            </li>
-                        );
-                    })}
-                </ul>
-                {loading && <div
-                    style={{
-                        position: 'absolute',
-                        top: 0,
-                        left: 0,
-                        width: '100%',
-                        height: '100%',
-                        backgroundColor: 'rgba(255, 255, 255, 0.8)',
-                        zIndex: 2,
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center'
-                    }}
-                >
-                    <Loader size={70}/>
-                </div>}
+          className="gn-card-grid"
+          style={{
+            listStyleType: "none",
+            padding: "0.5rem",
+            margin: 0,
+          }}
+        >
+          {resources.map((data, idx) => {
+            return (
+              <li style={{ padding: "0.25rem 0" }} key={data.pk + "-" + idx}>
+                <ResourceCard data={data} layoutCardsStyle="list" readOnly />
+              </li>
+            );
+          })}
+        </ul>
+        {loading && (
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              backgroundColor: "rgba(255, 255, 255, 0.8)",
+              zIndex: 2,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Loader size={70} />
+          </div>
+        )}
       </ResizableModal>
     </Portal>
   );
@@ -126,33 +109,21 @@ const ConnectedSyncResourcePlugin = connect(
   }
 )(SyncResourcePlugin);
 
-const SyncButton = ({ onClick, size, resource }) => {
-  const handleClickButton = () => {
-    onClick([resource]);
-  };
-
-  return (
-    <Button variant="danger" size={size} onClick={handleClickButton}>
-      syncisieren bitte
-    </Button>
-  );
-};
-
-const ConnectedSyncButton = connect(
+/* const ConnectedSyncButton = connect(
   createSelector([getResourceData], (resource) => ({
     resource,
   })),
   {
     onClick: setControlProperty.bind(null, ProcessTypes.SYNC_RESOURCE, "value"),
   }
-)(SyncButton);
+)(SyncResourcePlugin); */
 
 export default createPlugin("SyncResource", {
   component: ConnectedSyncResourcePlugin,
   containers: {
-    ActionNavbar: {
+    Catalogue: {
       name: "SyncResource",
-      Component: ConnectedSyncButton,
+      Component: SyncResourcePlugin,
     },
   },
   epics: {},
