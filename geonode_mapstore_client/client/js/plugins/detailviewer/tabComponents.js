f523644b680da1c775c5ea15a31f678cccd03380
Henning Bredel <h.bredel@52north.org>
2024-12-04 14:29:40 +0100

    Fix import from the future (i.e. master)

--- geonode_mapstore_client/client/js/plugins/detailviewer/tabComponents.js
+++ geonode_mapstore_client/client/js/plugins/detailviewer/tabComponents.js
@@ -6,20 +6,16 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { connect } from 'react-redux';
 import DetailsLocations from '@js/components/DetailsPanel/DetailsLocations';
 import DetailsAssets from '@js/components/DetailsPanel/DetailsAssets';
 import DetailsAttributeTable from '@js/components/DetailsPanel/DetailsAttributeTable';
 import DetailsLinkedResources from '@js/components/DetailsPanel/DetailsLinkedResources';
-import DetailsSettings from '@js/components/DetailsPanel/DetailsSettings';
-import { updateResourceProperties } from '@js/actions/gnresource';
 
 const tabComponents = {
     'attribute-table': DetailsAttributeTable,
     'linked-resources': DetailsLinkedResources,
     'locations': DetailsLocations,
-    'assets': DetailsAssets,
-    'settings': connect(() => ({}), { onChange: updateResourceProperties })(DetailsSettings)
+    'assets': DetailsAssets
 };
 
 export default tabComponents;
